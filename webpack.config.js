6683e77c702f31d0ad84aa09a34f28b9bfa69e5a
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-12 17:29:33 -0600

    Configuración de Webpack para CSS en React

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -29,6 +30,10 @@ module.exports = {
         use: {
           loader: 'html-loader'
         }
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
@@ -36,7 +41,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
